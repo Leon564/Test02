df96c0e1a63227679bb18cdf9516502f5bde8a3c
Nelson Leon <nleon564@hotmail.com>
2020-12-09 22:15:42 -0600

    extends

--- test/src/main/java/com/leon564/test/TestApplication.java
+++ test/src/main/java/com/leon564/test/TestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.leon564.test.controladores"})
-public class TestApplication extends SpringBootServletInitializer {
+public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
