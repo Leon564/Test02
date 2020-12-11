2610597b228766efd3becc6675de9703af8eafc9
Nelson Leon <nleon564@hotmail.com>
2020-12-10 22:45:19 -0600

    p

--- test/src/main/java/com/leon564/test/controladores/ProductosController.java
+++ test/src/main/java/com/leon564/test/controladores/ProductosController.java
@@ -1,5 +1,49 @@
 package com.leon564.test.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.leon564.test.entidades.Productos;
+import com.leon564.test.entidades.user;
+import com.leon564.test.repositorios.RepoProductos;
+import com.leon564.test.repositorios.RepoUser;
+
+@Controller
 public class ProductosController {
 
+	
+	@Autowired
+	RepoProductos Repo;
+	
+	@GetMapping("/registerPr")
+	public String nuevo() {
+		return "Producto/RegisterProducto";
+	}
+	
+	@PostMapping("/registrarP")
+	public String registrar( 
+			//@Valid
+			@ModelAttribute("producto")Productos producto,
+			BindingResult result ) {
+		
+		Repo.save(producto);
+		
+	
+		return "redirect:/";
+		
+		
+	}
+	
+	@GetMapping("/productos")
+	public String list(Model model) {
+		model.addAttribute("producto", Repo.findAll());
+		return "Producto/Productos";
+	}
+	
+	
 }
