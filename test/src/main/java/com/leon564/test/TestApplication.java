f8a60f0ebb69d4481bcb28095301a61b1dabb2b6
Nelson Leon <nleon564@hotmail.com>
2020-12-11 22:49:14 -0600

    xd

--- test/src/main/java/com/leon564/test/TestApplication.java
+++ test/src/main/java/com/leon564/test/TestApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
-@ComponentScan({"com.leon564.test.controladores"})
+//@SpringBootApplication
+//@ComponentScan({"com.leon564.test.controladores"})
 public class TestApplication {
 
 	public static void main(String[] args) {
